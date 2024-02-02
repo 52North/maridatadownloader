5c3526e11a56d65a51d9d934824e74dc3b73ae43
iquaresma <i.quaresma@52north.org>
2024-02-02 15:04:28 +0100

    Add docstrings cmt class and download method

--- maridatadownloader/copernicus_marine_toolbox.py
+++ maridatadownloader/copernicus_marine_toolbox.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 class DownloaderCopernicusMarineToolboxapi(DownloaderBase):
     """
-    
+    Downloader class for CMEMs data using the Copernicus Marine Toolbox api
     
     https://pypi.org/project/copernicusmarine/
     """
@@ -22,7 +22,22 @@ class DownloaderCopernicusMarineToolboxapi(DownloaderBase):
         super().__init__('cmtapi', username=cmems_username, password=cmems_password,**kwargs)
         self.client = copernicusmarine.login(username=cmems_username, password=cmems_password)
     
-    def download(self, sel_dict=None, file_out=None, **kwargs):
+    def download(self, sel_dict=None, file_out=None):
+        """
+        :param sel_dict: dict
+            Dataset_id
+            Coordinate selection by value, {'longitude': [min_value, max_value]} e.g. {'longitude': [10.25, 20.75]}
+            Time
+            variables
+        :param file_out:
+            File name used to save the dataset as NetCDF file. No file is saved without specifying file_out
+        :return: xarray.Dataset    
+            
+        Reference:
+         - https://help.marine.copernicus.eu/en/articles/7949409-copernicus-marine-toolbox-introduction
+         - https://help.marine.copernicus.eu/en/articles/8612591-switching-from-current-to-new-services
+          
+        """
         dataset = copernicusmarine.open_dataset(
             dataset_id =  sel_dict["dataset_id"],
             minimum_longitude = sel_dict["longitude"][0],
@@ -34,9 +49,8 @@ class DownloaderCopernicusMarineToolboxapi(DownloaderBase):
             variables = sel_dict["variables"]
         )
         
+        # Save dataset into file
         if file_out:
             logger.info(f"Save dataset to '{file_out}'")
             dataset.to_netcdf(file_out)     
         return dataset
-
-
