65fc96ff15459357ff3849f7ebc5c0be7ecb642a
iquaresma <i.quaresma@52north.org>
2024-02-05 19:52:22 +0100

    Adapt dict input format to match other classes and add keyErrortreatment

--- maridatadownloader/copernicus_marine_toolbox.py
+++ maridatadownloader/copernicus_marine_toolbox.py
@@ -1,15 +1,10 @@
-import io
 import logging
 
-import xarray as xr
 import copernicusmarine
-
 
 from maridatadownloader.base import DownloaderBase
 
 logger = logging.getLogger(__name__)
-
-
 
 class DownloaderCopernicusMarineToolboxapi(DownloaderBase):
     """
@@ -22,11 +17,12 @@ class DownloaderCopernicusMarineToolboxapi(DownloaderBase):
         super().__init__('cmtapi', username=cmems_username, password=cmems_password,**kwargs)
         self.client = copernicusmarine.login(username=cmems_username, password=cmems_password)
     
-    def download(self, sel_dict=None, file_out=None):
+    def download(self, parameters=None, sel_dict=None, file_out=None):
         """
+        :param parameters: list
         :param sel_dict: dict
             Dataset_id
-            Coordinate selection by value, {'longitude': [min_value, max_value]} e.g. {'longitude': [10.25, 20.75]}
+            Coordinate selection by value, e.g. {'longitude': slice(10.25, 20.75)} or {'longitude': 10.25}
             Time
             variables
         :param file_out:
@@ -36,19 +32,26 @@ class DownloaderCopernicusMarineToolboxapi(DownloaderBase):
         Reference:
          - https://help.marine.copernicus.eu/en/articles/7949409-copernicus-marine-toolbox-introduction
          - https://help.marine.copernicus.eu/en/articles/8612591-switching-from-current-to-new-services
-          
+         
         """
-        dataset = copernicusmarine.open_dataset(
-            dataset_id =  sel_dict["dataset_id"],
-            minimum_longitude = sel_dict["longitude"][0],
-            maximum_longitude = sel_dict["longitude"][1],
-            minimum_latitude = sel_dict["latitude"][0],
-            maximum_latitude = sel_dict["latitude"][1],
-            start_datetime = sel_dict["time"][0],
-            end_datetime = sel_dict["time"][1],
-            variables = sel_dict["variables"]
-        )
         
+        # Initialize dataset object 
+        dataset =  None 
+        
+        try:
+            dataset = copernicusmarine.open_dataset(
+                dataset_id =  sel_dict["dataset_id"],
+                minimum_longitude = sel_dict["longitude"].start,
+                maximum_longitude = sel_dict["longitude"].stop,
+                minimum_latitude = sel_dict["latitude"].start,
+                maximum_latitude = sel_dict["latitude"].stop,
+                start_datetime = sel_dict["time"].start,
+                end_datetime = sel_dict["time"].stop,
+                variables = parameters
+            )
+        except KeyError:
+            raise ValueError("Make sure to pass all the necessary values to subset the data (dataset_id, and lat, long, time intervals)")
+            
         # Save dataset into file
         if file_out:
             logger.info(f"Save dataset to '{file_out}'")
